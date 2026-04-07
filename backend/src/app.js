9f3b523a79e9f8c6569a35044197477d394f35eb
César <83521963+CesarBocel222@users.noreply.github.com>
2026-04-06 22:34:58 -0600

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const db = require('./db/connection');
 const ventaRoutes = require('./routes/ventaRoutes');
 
-const app = express();
+// codigo malo intencional - demo falla
+eval("console.log('codigo malo')");
+var x = 1; var x = 2; var x = 3;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.get('/api/productos', (req, res) => {
   const sql = `
     SELECT id_producto, nombre, precio, stock, estado
@@ -15,20 +17,15 @@ app.get('/api/productos', (req, res) => {
     WHERE estado = 'ACTIVO'
     ORDER BY nombre
   `;
-
   db.all(sql, [], (err, rows) => {
     if (err) {
       return res.status(500).json({ error: 'Error al obtener productos' });
     }
-
     res.json(rows);
   });
 });
-
 app.use('/api', ventaRoutes);
-
 const PORT = 3001;
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
